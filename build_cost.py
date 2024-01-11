939e2648f1cda6ba363aa53eeb4d1e79a1a80a3e
이정훈 <105867850+MeritEnding@users.noreply.github.com>
2024-01-11 22:01:21 +0900

    Update build_cost.py

--- build_cost.py
+++ build_cost.py
@@ -1,4 +1,4 @@
-// H(x) =Wx +b
+# H(x) =Wx +b
 x_data = [1,2,3,4,5]
 y_data = [1,2,3,4,5]
 
@@ -6,14 +6,14 @@ W= tf.Variable(2.9)
 b= tf.Variable(0.5)
 hypothesis =W *x_data +b
 
-// cost(W,b) 에러 제곱의 평균
+# cost(W,b) 에러 제곱의 평균
 cost =tf.reduce_mean(tf.square(hypotehsis - y_data))
 
-// tf.reduce_mean()
+# tf.reduce_mean()
 v =[1.,2.,3.,4.] 
 tf.reduce_mean(v) // 2.5
 
-// tf.square() 제곱
+# tf.square() 제곱
 tf.square(3)  //9
 
 
